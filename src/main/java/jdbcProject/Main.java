6f23728109183a8a6db99acd82ea01a081e4080a
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-07 11:12:59 -0400

    complete tutorial

--- src/main/java/jdbcProject/Main.java
+++ src/main/java/jdbcProject/Main.java
@@ -44,12 +44,7 @@ import java.sql.Statement;
 //
 //}
 
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//​
+// B
 public class Main {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
